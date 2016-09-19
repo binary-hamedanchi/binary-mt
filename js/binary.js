67f71b01c905bc3e9732a2421ad6d75393ebc2f3
binary-hamedanchi <hamedanchi@gmail.com>
2016-09-19 18:25:07 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -18299,16 +18299,12 @@ var BinarySocket = new BinarySocketClass();
     };
 
     var getAccountType = function(group) {
-        var accType = '';
-        if(group) {
-            if(/^demo/.test(group)) accType = 'demo';
-            else {
-                var landing_company = group.split('\\')[1];
-                if(landing_company === 'vanuatu') accType = 'financial';
-                else if(landing_company === 'costarica') accType = 'gaming';
-            }
-        }
-        return accType;
+        var typeMap = {
+            'virtual'  : 'demo',
+            'vanuatu'  : 'financial',
+            'costarica': 'gaming'
+        };
+        return group ? (typeMap[group.split('\\')[1]] || '') : '';
     };
 
     var findInSection = function(accType, selector) {
