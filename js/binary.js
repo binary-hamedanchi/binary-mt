56d33744ffde99660941e763d8cf7d3336fdd6b6
binary-hamedanchi <hamedanchi@gmail.com>
2016-11-28 14:38:20 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -17823,13 +17823,12 @@ function BinarySocketClass() {
 
 var BinarySocket = new BinarySocketClass();
 
-;var FinancialAssessmentws = (function() {
+;var FinancialAssessmentws = (function(){
    "use strict";
 
-    var financial_assessment = {};
-
-    var init = function() {
+    var init = function(){
         if (checkIsVirtual()) return;
+        LocalizeText();
         $("#assessment_form").on("submit",function(event) {
             event.preventDefault();
             submitForm();
@@ -17838,99 +17837,96 @@ var BinarySocket = new BinarySocketClass();
         BinarySocket.send({get_financial_assessment : 1});
     };
 
-    var submitForm = function() {
-        $('#submit').attr('disabled', 'disabled');
-
-        if (!validateForm()) {
-            setTimeout(function() { $('#submit').removeAttr('disabled'); }, 1000);
-            return;
-        }
-
-        var hasChanged = false;
-        Object.keys(financial_assessment).forEach(function(key) {
-            if ($('#' + key).length && $('#' + key).val() != financial_assessment[key]) {
-                hasChanged = true;
-            }
+    // For translating strings
+    var LocalizeText = function(){
+        $("#heading").text(text.localize($("#heading").text()));
+        $('#heading_risk').text(text.localize($("#heading_risk").text()));
+        $('#high_risk_classification').text(text.localize($('#high_risk_classification').text()));
+        document.getElementsByTagName('legend')[0].innerHTML = text.localize(document.getElementsByTagName('legend')[0].innerHTML);
+        if (document.getElementsByTagName('legend')[1]) document.getElementsByTagName('legend')[1].innerHTML = text.localize(document.getElementsByTagName('legend')[1].innerHTML);
+        $("#assessment_form label").each(function(){
+            var ele = $(this);
+            ele.text(text.localize(ele.text()));
         });
-        if (Object.keys(financial_assessment).length === 0) hasChanged = true;
-        if (!hasChanged) {
-            showFormMessage('You did not change anything.', false);
-            setTimeout(function() { $('#submit').removeAttr('disabled'); }, 1000);
+        $("#assessment_form option").each(function(){
+            var ele = $(this);
+            ele.text(text.localize(ele.text()));
+        });
+        $("#warning").text(text.localize($("#warning").text()));
+        $("#submit").text(text.localize($("#submit").text()));
+    };
+
+    var submitForm = function(){
+        if(!validateForm()){
             return;
         }
-
-        showLoadingImage($('#form_message'));
+        $('#submit').attr('disabled', 'disabled');
         var data = {'set_financial_assessment' : 1};
-        $('#assessment_form select').each(function() {
-            financial_assessment[$(this).attr('id')] = data[$(this).attr('id')] = $(this).val();
+        showLoadingImage($('#form_message'));
+        $('#assessment_form select').each(function(){
+            data[$(this).attr("id")] = $(this).val();
         });
         BinarySocket.send(data);
     };
 
-    var validateForm = function() {
+    var validateForm = function(){
         var isValid = true,
             errors = {};
-        clearErrors();
-        $('#assessment_form select').each(function() {
-            if (!$(this).val()) {
+        $('.errorfield').each(function() { $(this).text(''); });
+        $('#assessment_form select').each(function(){
+            if(!$(this).val()){
                 isValid = false;
                 errors[$(this).attr("id")] = text.localize('Please select a value');
             }
         });
-        if (!isValid) {
+        if(!isValid){
             displayErrors(errors);
         }
 
         return isValid;
     };
 
-    var showLoadingImg = function() {
+    var showLoadingImg = function(){
         showLoadingImage($('<div/>', {id: 'loading', class: 'center-text'}).insertAfter('#heading'));
         $("#assessment_form").addClass('invisible');
     };
 
-    var hideLoadingImg = function(show_form) {
+    var hideLoadingImg = function(show_form){
         $("#loading").remove();
-        if (typeof show_form === 'undefined') {
+        if(typeof show_form === 'undefined'){
             show_form = true;
         }
-        if (show_form) {
+        if(show_form)
             $("#assessment_form").removeClass('invisible');
-        }
     };
 
-    var responseGetAssessment = function(response) {
+    var responseGetAssessment = function(response){
         hideLoadingImg();
-        financial_assessment = response.get_financial_assessment;
-        for(var key in response.get_financial_assessment) {
-            if (key) {
+        for(var key in response.get_financial_assessment){
+            if(key){
                 var val = response.get_financial_assessment[key];
-                $('#' + key).val(val);
+                $("#"+key).val(val);
             }
         }
     };
 
-    var clearErrors = function() {
-        $('.errorfield').each(function() { $(this).text(''); });
-    };
-
-    var displayErrors = function(errors) {
+    var displayErrors = function(errors){
         var id;
-        clearErrors();
-        for(var key in errors) {
+        $(".errorfield").each(function(){$(this).text('');});
+        for(var key in errors){
             if(key){
                 var error = errors[key];
-                $('#error' + key).text(text.localize(error));
+                $("#error"+key).text(text.localize(error));
                 if (!id) id = key;
             }
         }
         hideLoadingImg();
         $('html, body').animate({
-            scrollTop: $('#' + id).offset().top
+            scrollTop: $("#"+id).offset().top
         }, 'fast');
     };
 
-    var apiResponse = function(response) {
+    var apiResponse = function(response){
         if (checkIsVirtual()) return;
         if (response.msg_type === 'get_financial_assessment'){
             responseGetAssessment(response);
@@ -17945,8 +17941,8 @@ var BinarySocket = new BinarySocketClass();
         }
     };
 
-    var checkIsVirtual = function() {
-        if (page.client.is_virtual()) {
+    var checkIsVirtual = function(){
+        if(page.client.is_virtual()) {
             $("#assessment_form").addClass('invisible');
             $('#response_on_success').addClass('notice-msg center-text').removeClass('invisible').text(text.localize('This feature is not relevant to virtual-money accounts.'));
             hideLoadingImg(false);
@@ -17981,10 +17977,11 @@ var BinarySocket = new BinarySocketClass();
     };
 
     return {
-        init       : init,
-        apiResponse: apiResponse,
-        submitForm : submitForm,
-        onLoad     : onLoad,
+        init : init,
+        apiResponse : apiResponse,
+        submitForm: submitForm,
+        LocalizeText: LocalizeText,
+        onLoad: onLoad,
     };
 }());
 
@@ -18378,12 +18375,17 @@ var BinarySocket = new BinarySocketClass();
     // --------------------------
     // ----- Tab Management -----
     // --------------------------
+    var getActiveTab = function() {
+        var activeTab = (page.url.location.hash.substring(1) || '').toLowerCase();
+        if (!activeTab || !/demo|financial|volatility/.test(activeTab)) {
+            activeTab = 'demo';
+        }
+        return activeTab;
+    };
+
     var displayTab = function(tab) {
         if(!tab) {
-            tab = (page.url.location.hash.substring(1) || '').toLowerCase();
-            if(!tab || !/demo|financial|volatility/.test(tab)) {
-                tab = 'demo';
-            }
+            tab = getActiveTab();
         }
         if((/financial/.test(tab) && !hasFinancialCompany) || (/volatility/.test(tab) && !hasGamingCompany)) {
             tab = 'demo';
@@ -18513,14 +18515,19 @@ var BinarySocket = new BinarySocketClass();
                     MetaTraderData.requestLoginDetails(obj.login);
                 }
             });
-        } else {
+        }
+        if (!mt5Accounts.hasOwnProperty(getActiveTab())) {
             displayTab();
         }
     };
 
     var responseLoginDetails = function(response) {
         if(response.hasOwnProperty('error')) {
-            return showAccountMessage(mt5Logins[response.echo_req.login], response.error.message);
+            showAccountMessage(mt5Logins[response.echo_req.login], response.error.message);
+            if (mt5Logins[response.echo_req.login] === getActiveTab()) {
+                displayTab();
+            }
+            return;
         }
 
         var accType = MetaTrader.getAccountType(response.mt5_get_settings.group);
